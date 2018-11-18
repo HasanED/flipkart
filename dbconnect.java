9529fde5a17c1215814ec48eb9c078affbe34445
Hasan <hasanrocking@gmail.com>
2018-11-18 06:22:01 +0530

    updated in master branch

--- dbconnect.java
+++ dbconnect.java
@@ -1,1 +1,2 @@
-hi guys my name is hasan and this file contains db connection logic
+hi guys my name is hasan and this file contains db connection logici
+hello i am chnaging the code in master brnach
