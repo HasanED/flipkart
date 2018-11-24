28fa7cca4f2c8329ad5edef214996fdc1d89902f
Hasan <hasanrocking@gmail.com>
2018-11-24 17:31:54 +0530

    eclipse changes

--- dbconnect.java
+++ dbconnect.java
@@ -3,6 +3,4 @@ hi guys my name is hasan and this file contains db connection logici
 hello i am chnaging the code in master brnach
 
 hi guys my name is hasan and this file contains db connection logic
-hello i am updating this java code from devlopment branch
-again i have updated 
-
+hello guys i am changing the source code in eclips ide
